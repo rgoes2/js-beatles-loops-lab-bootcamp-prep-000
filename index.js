32ca38eb30e5b7c229ae801063c7f135aedd4a5c
Renan Góes <renangoes12@gmail.com>
2018-08-07 00:28:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay (musicians,instruments){
+  var newArray = [];
+  for (var i = 0; i < 4; i++){
+  
+  newArray[i] = `${musicians[i]} plays ${instruments[i]}`; 
+  }
+  return newArray;
+}
+function JohnLennonFacts (facts){
+  var countdown = facts.length
+  while (countdown > 0){
+    facts[countdown] = `${facts[countdown]}!!!`;
+    --countdown
+  }
+  return facts
+}
